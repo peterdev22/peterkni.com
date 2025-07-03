eb4ba86a799974749868bf499a0c322fd1c5b85b
peterdev22 <95014170+peterdev22@users.noreply.github.com>
2025-07-03 17:39:33 +0930

    hdr works, now testing camera

--- src/lib/components/ModelScene.svelte
+++ src/lib/components/ModelScene.svelte
@@ -16,8 +16,8 @@
 
 <Environment url="/assets/3d/brown_photostudio_02_1k.hdr" />
 
-<!-- <T.PerspectiveCamera makeDefault position={[1, 1, 1]} fov={30}>
-  <OrbitControls
+<T.PerspectiveCamera makeDefault position={[1, 1, 1]} fov={30}>
+  <!-- <OrbitControls
     autoRotate
     autoRotateSpeed={0.5}
     minDistance={0.5}
@@ -27,11 +27,11 @@
     target={[0, 0.35, 0]}
   >
     <Gizmo />
-  </OrbitControls>
+  </OrbitControls> -->
 </T.PerspectiveCamera>
 
-<T.GridHelper args={[10, 10]} position.y={-0.001}/>
+<!-- <T.GridHelper args={[10, 10]} position.y={-0.001}/> -->
 
-<ContactShadows scale={1} blur={2} far={10} opacity={0.7} /> -->
+<!-- <ContactShadows scale={1} blur={2} far={10} opacity={0.7} /> -->
 
 <!-- <GLTF url={source} {dracoLoader} /> -->

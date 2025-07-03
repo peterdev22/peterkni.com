98c8031c45dee8de0dd3ce0d70d9f897e5a2248e
peterdev22 <95014170+peterdev22@users.noreply.github.com>
2025-07-03 17:33:55 +0930

    error occurs only when modelScene is used

--- src/lib/components/ModelScene.svelte
+++ src/lib/components/ModelScene.svelte
@@ -14,7 +14,7 @@
   let { source } = $props();
 </script>
 
-<Environment url="/assets/3d/brown_photostudio_02_1k.hdr" />
+<!-- <Environment url="/assets/3d/brown_photostudio_02_1k.hdr" />
 
 <T.PerspectiveCamera makeDefault position={[1, 1, 1]} fov={30}>
   <OrbitControls
@@ -32,6 +32,6 @@
 
 <T.GridHelper args={[10, 10]} position.y={-0.001}/>
 
-<ContactShadows scale={1} blur={2} far={10} opacity={0.7} />
+<ContactShadows scale={1} blur={2} far={10} opacity={0.7} /> -->
 
 <!-- <GLTF url={source} {dracoLoader} /> -->
